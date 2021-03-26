4d642f7d6403d449a4c9b0cfc67296a7379cb68e
Andrew Coughlin-MSFT <ancoughl@microsoft.com>
2021-03-26 15:55:26 -0500

    updates from today

---
+++ Azure.Policy/main-deployment/deploy.ps1
@@ -0,0 +1,12 @@
+$BaseDir = 'C:\GitHub\Azure\Azure.Policy'
+$Params = @{
+    Name                  = 'ActionGroupsPolicy'
+    ManagementGroupId     = '6f34bc28-ce0e-4936-a836-9730a5929bf7'
+    Location              = 'centralus'
+    TemplateParameterFile = "$BaseDir\main-deployment\linkedtemplate.paramters.json"
+    TemplateFile          = "$BaseDir\main-deployment\azuredeployment.json"
+}
+New-AzManagementGroupDeployment @Params -Verbose
+
+#New-AzResourceGroupDeployment -ResourceGroupName "RGName" -TemplateUri "https://raw.githubusercontent.com/Andrew-Coughlin-MSFT/Azure/master/Azure.Policy/templates/azuredeployment.json"
+# -QueryString "foo"

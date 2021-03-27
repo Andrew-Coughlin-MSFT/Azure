7e3779034bedaf965d5057049eb1fc528c8197f2
ancoughl@microsoft.com <ancoughl@microsoft.com>
2021-03-27 16:17:57 -0500

    checking in

--- ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/main-deployment/deploy.ps1
+++ ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/main-deployment/deploy.ps1
@@ -2,9 +2,8 @@ $BaseDir = 'C:\Azure\Azure\ArmTemplates\AzureMonitor\Templates\azure-monitor\Act
 $Params = @{
     Name                  = 'ActivityMonitorAlerts'
     Location              = 'centralus'
-    Subscription          = 'bde5ac7d-d5cc-4e8a-ae6c-73788d8a7af5'
-    activityLogAlertName  = ''
     TemplateParameterFile = "$BaseDir\main-deployment\azuredeployment.json"
     TemplateFile          = "$BaseDir\\main-deployment\azuredeployment.parameters.json"
 }
-New-AzManagementGroupDeployment @Params -Verbose
+##New-AzManagementGroupDeployment @Params -Verbose
+New-AzDeployment @Params -Verbose

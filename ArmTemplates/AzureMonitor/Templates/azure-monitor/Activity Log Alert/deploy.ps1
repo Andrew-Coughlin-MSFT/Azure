bce5908b58f4f4947b62f8fa753c5039224e5e1a
Andrew Coughlin-MSFT <ancoughl@microsoft.com>
2021-03-28 11:23:42 -0500

    updated script

--- ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/deploy.ps1
+++ ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/deploy.ps1
@@ -1,5 +1,5 @@
 <#
-        .Disclaimer
+        .DISCLAIMER
         MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
         Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
         in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -9,9 +9,10 @@
         FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
         IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
         SOFTWARE
+
         .SYNOPSIS
         Creates Azure Monitor Alerts for Azure CIS 1.1.0. Creates the requirements for a resource group and if needed action group.
-
+        
         .DESCRIPTION
         Creates Azure Monitor Alerts for Azure CIS 1.1.0.:
         5.2.1. Ensure that Activity Log Alert exists for Create Policy Assignment

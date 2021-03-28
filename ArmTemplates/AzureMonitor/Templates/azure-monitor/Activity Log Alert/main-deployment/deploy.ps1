2c5fdc608a3f66d8566b83c558cbb829dd0ac289
ancoughl@microsoft.com <ancoughl@microsoft.com>
2021-03-28 10:29:59 -0500

    Pushing Latest updates

--- ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/main-deployment/deploy.ps1
+++ ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/main-deployment/deploy.ps1
@@ -1,13 +1,28 @@
 <#
         .SYNOPSIS
-        Adds a file name extension to a supplied name.
+        Creates Azure Monitor Alerts for Azure CIS 1.1.0. Creates the requirements for a resource group and if needed action group.
 
         .DESCRIPTION
-        Adds a file name extension to a supplied name.
-        Takes any strings for the file name or extension.
+        Creates Azure Monitor Alerts for Azure CIS 1.1.0.:
+        5.2.1. Ensure that Activity Log Alert exists for Create Policy Assignment
+        5.2.2. Ensure that Activity Log Alert exists for Create or Update Network Security Group
+        5.2.3. Ensure that Activity Log Alert exists for Delete Network Security Group
+        5.2.4. Ensure that Activity Log Alert exists for Create or Update Network Security Group Rule
+        5.2.5. Ensure that activity log alert exists for the Delete Network Security Group Rule
+        5.2.6. Ensure that Activity Log Alert exists for Create or Update Security Solution
+        5.2.7. Ensure that Activity Log Alert exists for Delete Security Solution
+        5.2.8. Ensure that Activity Log Alert exists for Create or Update or Delete SQL Server Firewall Rule
+        5.2.9. Ensure that Activity Log Alert exists for Update Security Policy
+
+        Creates the requirements for a resource group and if needed action group.
 
         .PARAMETER bSingleSubscription
+        (Optional)
         Specifies if the script should run across all subscriptions or only one.  True = single subscription, False = multiple subscription.
+
+        .PARAMETER AzureRegion
+        (Optional)
+        Specifies the azure region to create the action group and resource group.
 
         .EXAMPLE
         PS> deploy.ps1 
@@ -15,19 +30,25 @@
         This example will run against all subscriptions you have permissions to create Azure Monitor alerts.
 
         .EXAMPLE
-        PS> deploy.ps1 -bSingleSubscription $true
+        PS> deploy.ps1 -AzureRegion "centralus"
 
-        This example will run against only 1 subscription you specify based on subscription ID.
+        This example will run against all subscriptions you have permissions to create Azure Monitor alerts and create resources in the central us region.
 
-        .LINK
-        Online version: http://www.fabrikam.com/extension.html
+        .EXAMPLE
+        PS> deploy.ps1 -bSingleSubscription $true -AzureRegion "centralus" -AzsubScriptionID "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+
+        This example will run against only 1 subscription, create resources in central us region and run against the subscription you specify.
 
     #>
 
 Param
 (
     [Parameter(Position = 0)]
-    [bool] $bSingleSubscription = $false
+    [bool] $bSingleSubscription = $false,
+    [Parameter(Position = 1)]
+    [string] $AzureRegion,
+    [Parameter(Position = 2)]
+    [string] $AzsubScriptionID
 )
 function Create_Action_Group {
     Param
@@ -47,14 +68,20 @@ function Create_Action_Group {
         $PSCmdlet.WriteError($_)
         Break
     }
-    
-    
+}
+function AzAccountLogin() {
+    $AzContext = Get-AzContext
+
+    if (!$AzContext) {
+        Write-Host "Connecting to Azure"
+        Connect-AzAccount
+    } 
 }
 
 ##################################Configuration Area ##########################################################
 #Configure these variables before executing this script
 
-$AlertRuleActionGroupName = "email_me" #action group, where you want the emails to go to when these alerts fire
+$AlertRuleActionGroupName = "Activity_Log_Alerts" #action group, where you want the emails to go to when these alerts fire
 #Alteratively you can create the action group a different way just need to reference the name here and it won't recreate it.
 $myResourceGroupName = "rg-alerts" #resource group where you want the alerts to be created
 $emailAddress = "ancoughlin@microsoft.com"  #email address to send the alerts when the alert is triggered
@@ -65,22 +92,38 @@ try {
     $BaseDir = 'https://raw.githubusercontent.com/Andrew-Coughlin-MSFT/Azure/master/ArmTemplates/AzureMonitor/Templates'
     $templateUri = "$BaseDir/azure-monitor/Activity%20Log%20Alert/main-deployment/azuredeployment.json"
     
-
     if ($bSingleSubscription -eq $false) {
+        AzAccountLogin
         $subscriptions = Get-AzSubscription | Where-Object { $_.State -eq "Enabled" }
     }
     else {
-        $SubscriptionId = Read-Host "Type the Subscription Id"
-        $subscriptions = Get-AzSubscription -SubscriptionId $SubscriptionId
+        AzAccountLogin
+        if (([string]::IsNullOrEmpty($AzsubScriptionID))) {
+            
+            $SubscriptionId = Read-Host "Type the Subscription Id"
+            $subscriptions = Get-AzSubscription -SubscriptionId $SubscriptionId
+        }
+        else {
+            $SubscriptionId = $AzsubScriptionID
+            $subscriptions = Get-AzSubscription -SubscriptionId $SubscriptionId
+        }
     }
-
+    if ($subscriptions.Count -lt 1) {
+        Write-Output "No Subscriptions to run against." -BackgroundColor Yellow -ForegroundColor Black
+    }
     foreach ($subscription in $subscriptions) {
         Select-AzSubscription -SubscriptionID $subscription.Id
         Get-AzResourceGroup -Name $myResourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
         if ($notPresent) {
             # ResourceGroup doesn't exist
             Write-Output "Creating Resource Group."
-            $AzRegionName = Read-Host "Specify Azure Region Name (ex: centralus)"
+            if (([string]::IsNullOrEmpty($AzureRegion))) {
+                $AzRegionName = Read-Host "Specify Azure Region Name (ex: centralus)"
+            }
+            else {
+                $AzRegionName = $AzureRegion
+            }
+            #Create Resource Group
             New-AzResourceGroup -Location $AzRegionName -Name $myResourceGroupName
             Write-Output "Creating Action Group"
             Create_Action_Group -rgName $myResourceGroupName -actionGroupName $AlertRuleActionGroupName -Email $emailAddress
@@ -94,7 +137,7 @@ try {
                 Create_Action_Group -rgName $myResourceGroupName -actionGroupName $AlertRuleActionGroupName -Email $emailAddress
             }
         }
-        
+        #execute deployment for Azure Monitor Alerts
         $targetsub = $subscription.Id
         New-AzResourceGroupDeployment -ResourceGroupName $myResourceGroupName -TemplateUri $templateUri -targetsub $targetsub -AlertRuleActionGroupName $AlertRuleActionGroupName -Verbose
     }
@@ -102,4 +145,5 @@ try {
 catch {
     Write-Output $_.Exception.Message
 }
+
 

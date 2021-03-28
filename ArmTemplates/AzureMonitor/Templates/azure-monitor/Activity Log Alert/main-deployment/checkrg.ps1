80bba5517742a9af6358bf627b7f7878b3a56618
ancoughl@microsoft.com <ancoughl@microsoft.com>
2021-03-27 21:03:23 -0500

    checking in new version.

---
+++ ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/main-deployment/checkrg.ps1
@@ -0,0 +1,51 @@
+    <#
+        .SYNOPSIS
+        Adds a file name extension to a supplied name.
+
+        .DESCRIPTION
+        Adds a file name extension to a supplied name.
+        Takes any strings for the file name or extension.
+
+        .PARAMETER Name
+        Specifies the file name.
+
+        .PARAMETER Extension
+        Specifies the extension. "Txt" is the default.
+
+        .INPUTS
+        None. You cannot pipe objects to Add-Extension.
+
+        .OUTPUTS
+        System.String. Add-Extension returns a string with the extension or file name.
+
+        .EXAMPLE
+        PS> extension -name "File"
+        File.txt
+
+        .EXAMPLE
+        PS> extension -name "File" -extension "doc"
+        File.doc
+
+        .EXAMPLE
+        PS> extension "File" "doc"
+        File.doc
+
+        .LINK
+        Online version: http://www.fabrikam.com/extension.html
+
+        .LINK
+        Set-Item
+    #>
+
+Param
+(
+    [Parameter(Position = 0)]
+    [bool] $bSingleSubscription = $false
+)
+
+if ($bSingleSubscription -eq $false){
+    Write-Output "Multiple Azure Subscriptions"
+}
+else {
+    Write-Output "Single Azure Subscription"
+}
--- ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/main-deployment/deploy.ps1
+++ ArmTemplates/AzureMonitor/Templates/azure-monitor/Activity Log Alert/main-deployment/deploy.ps1
@@ -1,9 +1,113 @@
-$BaseDir = 'C:\Azure\Azure\ArmTemplates\AzureMonitor\Templates\azure-monitor\Activity Log Alert\'
+<#
+        .SYNOPSIS
+        Adds a file name extension to a supplied name.
 
-$resourceGroupName = "rg-alerts"
-$BaseDir = 'C:\Azure\Azure\ArmTemplates\AzureMonitor\Templates\azure-monitor\Activity Log Alert\'
-$templateUri = "$BaseDir\main-deployment\azuredeployment.json"
-$templateFile ="$BaseDir\\main-deployment\azuredeployment.parameters.json"
-#$location = "centralus"
+        .DESCRIPTION
+        Adds a file name extension to a supplied name.
+        Takes any strings for the file name or extension.
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri  -TemplateParameterFile $templateFile -Verbose
+        .PARAMETER Name
+        Specifies the file name.
+
+        .PARAMETER Extension
+        Specifies the extension. "Txt" is the default.
+
+        .INPUTS
+        None. You cannot pipe objects to Add-Extension.
+
+        .OUTPUTS
+        System.String. Add-Extension returns a string with the extension or file name.
+
+        .EXAMPLE
+        PS> extension -name "File"
+        File.txt
+
+        .EXAMPLE
+        PS> extension -name "File" -extension "doc"
+        File.doc
+
+        .EXAMPLE
+        PS> extension "File" "doc"
+        File.doc
+
+        .LINK
+        Online version: http://www.fabrikam.com/extension.html
+
+        .LINK
+        Set-Item
+    #>
+
+Param
+(
+    [Parameter(Position = 0)]
+    [bool] $bSingleSubscription = $false
+)
+function Create_Action_Group {
+    Param
+    (
+        [Parameter(Position = 0)]
+        [string] $rgName,
+        [Parameter(Position = 1)]
+        [string] $actionGroupName,
+        [Parameter(Position = 2)]
+        [string] $Email
+    )
+    try {
+        $EmailAddresses = New-AzActionGroupReceiver -Name "Email" -EmailReceiver -EmailAddress $Email
+        Set-AzActionGroup -Name $actionGroupName -ResourceGroupName $rgName -ShortName 'email' -Receiver $EmailAddresses
+    }
+    catch {
+        $PSCmdlet.WriteError($_)
+        Break
+    }
+    
+    
+}
+
+try {
+    $BaseDir = 'C:\Azure\Azure\ArmTemplates\AzureMonitor\Templates\azure-monitor\Activity Log Alert\'
+    $resourceGroupName = "rg-alerts"
+    $BaseDir = 'C:\Azure\Azure\ArmTemplates\AzureMonitor\Templates\azure-monitor\Activity Log Alert\'
+    $templateUri = "$BaseDir\main-deployment\azuredeployment.json"
+    $AlertRuleActionGroupName = "email_me"
+    ##$templateFile ="$BaseDir\\main-deployment\azuredeployment.parameters.json"
+    $myResourceGroupName = "rg-alerts"
+    $emailAddress = "ancoughlin@microsoft.com"
+
+    if ($bSingleSubscription -eq $false) {
+        $subscriptions = Get-AzSubscription | Where-Object { $_.State -eq "Enabled" }
+    }
+    else {
+        $SubscriptionId = Read-Host "Type the Subscription Id"
+        $subscriptions = Get-AzSubscription -SubscriptionId $SubscriptionId
+    }
+
+    foreach ($subscription in $subscriptions) {
+        Select-AzSubscription -SubscriptionID $subscription.Id
+        Get-AzResourceGroup -Name $myResourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+        if ($notPresent) {
+            # ResourceGroup doesn't exist
+            Write-Output "Creating Resource Group."
+            $AzRegionName = Read-Host "Specify Azure Region Name (ex: centralus)"
+            New-AzResourceGroup -Location $AzRegionName -Name $myResourceGroupName
+            Write-Output "Creating Action Group"
+            Create_Action_Group -rgName $myResourceGroupName -actionGroupName $AlertRuleActionGroupName -Email $emailAddress
+        }
+        else {
+            Write-Output "Resource Group Already Created, going to next step."
+            Get-AzActionGroup -Name $myResourceGroupName -ResourceGroupName $myResourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+            if ($notPresent) {
+                ##Action Group Doesn't exist
+                Write-Output "Creation Action Group"
+                Create_Action_Group -rgName $myResourceGroupName -actionGroupName $AlertRuleActionGroupName -Email $emailAddress
+            }
+        }
+        
+        $targetsub = $subscription.Id
+        New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -targetsub $targetsub -AlertRuleActionGroupName $AlertRuleActionGroupName -Verbose
+    }
+}
+catch {
+    Write-Output $_.Exception.Message
+}
+

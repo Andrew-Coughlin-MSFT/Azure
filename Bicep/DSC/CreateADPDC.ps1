34f0c554a3902cc68f3bf43663311f2c03e33d1e
Andrew Coughlin <ancoughl@microsoft.com>
2022-06-22 06:32:38 -0500

    Included updated Module, updated Create ps script

--- Bicep/DSC/CreateADPDC.ps1
+++ Bicep/DSC/CreateADPDC.ps1
@@ -1,4 +1,4 @@
-configuration CreateADPDC
+﻿configuration CreateADPDC
 {
    param
    (
@@ -11,12 +11,13 @@ configuration CreateADPDC
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
     )
-
+    Write-Output $DomainName
+    Write-Output $Admincreds
     Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
     $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
     $InterfaceAlias=$($Interface.Name)
-
+    Write-Output $DomainCreds
     Node localhost
     {
         LocalConfigurationManager

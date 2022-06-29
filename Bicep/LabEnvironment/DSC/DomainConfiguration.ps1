03907494ea93b7177f4a6727a4aa3e4e30ae6d6e
Andrew Coughlin <ancoughl@microsoft.com>
2022-06-29 09:18:37 -0500

    Update script

--- Bicep/LabEnvironment/DSC/DomainConfiguration.ps1
+++ Bicep/LabEnvironment/DSC/DomainConfiguration.ps1
@@ -1,14 +1,20 @@
+param([string] $domainFQDN)
+
+
+$domain=$domainFQDN
+$domainDN = "DC=$($domain.replace(".", ",DC="))"
+
 ##Setup OUs for Domain
 Install-WindowsFeature -Name "RSAT-AD-PowerShell" -IncludeAllSubFeature
 
 Import-Module -Name ActiveDirectory
 
-New-ADOrganizationalUnit -Name "All Users" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
-New-ADOrganizationalUnit -Name "All Groups" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
-New-ADOrganizationalUnit -Name "All Server Accounts" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
-New-ADOrganizationalUnit -Name "All Servers" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
-New-ADOrganizationalUnit -Name "W2K19" -Path "OU=All Servers,DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
-New-ADOrganizationalUnit -Name "W2K22" -Path "OU=All Servers,DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
-New-ADOrganizationalUnit -Name "All Desktops" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
-New-ADOrganizationalUnit -Name "Autopilot Domain Join" -Path "OU=All Desktops,DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
-New-ADOrganizationalUnit -Name "AzureFiles" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Users" -Path $domainDN -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Groups" -Path $domainDN -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Server Accounts" -Path $domainDN -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Servers" -Path $domainDN -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "W2K19" -Path "OU=All Servers,$domainDN" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "W2K22" -Path "OU=All Servers,$domainDN" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Desktops" -Path $domainDN -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "Autopilot Domain Join" -Path "OU=All Desktops,$domainDN" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "AzureFiles" -Path $domainDN -ProtectedFromAccidentalDeletion $true

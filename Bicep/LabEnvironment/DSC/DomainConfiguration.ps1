e773033ab45054edebe3c4e88a46fdb480e860bd
Andrew Coughlin <ancoughl@microsoft.com>
2022-06-29 07:04:18 -0500

    Domain Configuration OUs

---
+++ Bicep/LabEnvironment/DSC/DomainConfiguration.ps1
@@ -0,0 +1,11 @@
+##Setup OUs for Domain
+
+New-ADOrganizationalUnit -Name "All Users" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Groups" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Server Accounts" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Servers" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "W2K19" -Path "OU=All Servers,DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "W2K22" -Path "OU=All Servers,DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "All Desktops" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "Autopilot Domain Join" -Path "OU=All Desktops,DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true
+New-ADOrganizationalUnit -Name "AzureFiles" -Path "DC=LAB,DC=LOCAL" -ProtectedFromAccidentalDeletion $true

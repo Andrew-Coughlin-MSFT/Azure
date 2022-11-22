ddc0faab6d5c0e3161871478b86f402393595195
Andrew Coughlin <ancoughl@microsoft.com>
2022-11-22 12:38:57 -0600

    .

--- Bicep/HybridDNSResolutionLab/DSC/ConfigureADBDC.ps1
+++ Bicep/HybridDNSResolutionLab/DSC/ConfigureADBDC.ps1
@@ -12,9 +12,12 @@
         [Int]$RetryIntervalSec=30
     )
 
-    Import-DscResource -ModuleName xActiveDirectory, xPendingReboot, xStorage, xNetworking
-    $shortDomainName = $DomainName.Substring(0,$DomainName.IndexOf('.'))
-    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${shortDomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface=Get-NetAdapter|Where-Object Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+    #Convert FQDN to Distinguished Name
+    $DN = 'DC=' + $DomainName.Replace('.',',DC=')
 
     Node localhost
     {
@@ -47,6 +50,14 @@
             DependsOn = "[WindowsFeature]DNS"
 	    }
 
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+	        DependsOn = "[WindowsFeature]DNS"
+        }
+
         xWaitforDisk Disk2
         {
                 DiskNumber = 2
@@ -54,8 +65,7 @@
                 RetryCount = $RetryCount
         }
 
-        xDisk ADDataDisk
-        {
+        xDisk ADDataDisk {
             DiskNumber = 2
             DriveLetter = "F"
             DependsOn = "[xWaitForDisk]Disk2"
@@ -79,46 +89,20 @@
         {
             Ensure = "Present"
             Name = "RSAT-AD-AdminCenter"
-            DependsOn = "[WindowsFeature]ADDSTools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
         }
 
-        xWaitForADDomain DscForestWait
-        {
-            DomainName = $shortDomainName
-            DomainUserCredential= $DomainCreds
-            RetryCount = $RetryCount
-            RetryIntervalSec = $RetryIntervalSec
-            DependsOn = "[WindowsFeature]ADAdminCenter"
-        }
         xADDomainController BDC
         {
-            DomainName = $shortDomainName
+            DomainName = $DomainName
             DomainAdministratorCredential = $DomainCreds
             SafemodeAdministratorPassword = $DomainCreds
             DatabasePath = "F:\NTDS"
             LogPath = "F:\NTDS"
             SysvolPath = "F:\SYSVOL"
-            DependsOn = "[xWaitForADDomain]DscForestWait"
+            DependsOn = @("[xDisk]ADDataDisk", "[WindowsFeature]ADDSInstall")
         }
-<#
-        Script UpdateDNSForwarder
-        {
-            SetScript =
-            {
-                Write-Verbose -Verbose "Getting DNS forwarding rule..."
-                $dnsFwdRule = Get-DnsServerForwarder -Verbose
-                if ($dnsFwdRule)
-                {
-                    Write-Verbose -Verbose "Removing DNS forwarding rule"
-                    Remove-DnsServerForwarder -IPAddress $dnsFwdRule.IPAddress -Force -Verbose
-                }
-                Write-Verbose -Verbose "End of UpdateDNSForwarder script..."
-            }
-            GetScript =  { @{} }
-            TestScript = { $false}
-            DependsOn = "[xADDomainController]BDC"
-        }
-#>
+
         xPendingReboot RebootAfterPromotion {
             Name = "RebootAfterDCPromotion"
             DependsOn = "[xADDomainController]BDC"

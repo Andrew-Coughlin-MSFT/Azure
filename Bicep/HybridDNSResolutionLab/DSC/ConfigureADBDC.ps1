edb046a578e8e7c1b06ce0945294163798d30f2a
Andrew Coughlin <ancoughl@microsoft.com>
2022-11-23 08:04:10 -0600

    .

--- Bicep/HybridDNSResolutionLab/DSC/ConfigureADBDC.ps1
+++ Bicep/HybridDNSResolutionLab/DSC/ConfigureADBDC.ps1
@@ -1,4 +1,4 @@
-﻿configuration ConfigureADBDC
+configuration ConfigureADBDC
 {
    param
     (
@@ -12,10 +12,9 @@
         [Int]$RetryIntervalSec=30
     )
 
-    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
+    Import-DscResource -ModuleName xActiveDirectory, xPendingReboot
+
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
-    $Interface=Get-NetAdapter|Where-Object Name -Like "Ethernet*"|Select-Object -First 1
-    $InterfaceAlias=$($Interface.Name)
 
     Node localhost
     {
@@ -24,72 +23,13 @@
             RebootNodeIfNeeded = $true
         }
         
-        WindowsFeature DNS 
-        { 
-            Ensure = "Present" 
-            Name = "DNS"		
-        }
-
-        Script EnableDNSDiags
-	    {
-      	    SetScript = { 
-		        Set-DnsServerDiagnostics -All $true
-                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
-            }
-            GetScript =  { @{} }
-            TestScript = { $false }
-	        DependsOn = "[WindowsFeature]DNS"
-        }
-
-	    WindowsFeature DnsTools
-	    {
-	        Ensure = "Present"
-            Name = "RSAT-DNS-Server"
-            DependsOn = "[WindowsFeature]DNS"
-	    }
-
-        xWaitforDisk Disk2
-        {
-                DiskNumber = 2
-                RetryIntervalSec =$RetryIntervalSec
-                RetryCount = $RetryCount
-        }
-
-        xDisk ADDataDisk {
-            DiskNumber = 2
-            DriveLetter = "F"
-            DependsOn = "[xWaitForDisk]Disk2"
-        }
-
-        WindowsFeature ADDSInstall
-        {
-            Ensure = "Present"
-            Name = "AD-Domain-Services"
-            DependsOn="[WindowsFeature]DNS" 
-        }
-
-        WindowsFeature ADDSTools
-        {
-            Ensure = "Present"
-            Name = "RSAT-ADDS-Tools"
-            DependsOn = "[WindowsFeature]ADDSInstall"
-        }
-
-        WindowsFeature ADAdminCenter
-        {
-            Ensure = "Present"
-            Name = "RSAT-AD-AdminCenter"
-            DependsOn = "[WindowsFeature]ADDSTools"
-        }
         xWaitForADDomain DscForestWait
         {
             DomainName = $DomainName
             DomainUserCredential= $DomainCreds
             RetryCount = $RetryCount
             RetryIntervalSec = $RetryIntervalSec
-            DependsOn = "[WindowsFeature]ADDSInstall"
         }
-        
         xADDomainController BDC
         {
             DomainName = $DomainName
@@ -98,8 +38,31 @@
             DatabasePath = "F:\NTDS"
             LogPath = "F:\NTDS"
             SysvolPath = "F:\SYSVOL"
-            DependsOn = @("[xWaitForADDomain]")
-
+            DependsOn = "[xWaitForADDomain]DscForestWait"
         }
+<#
+        Script UpdateDNSForwarder
+        {
+            SetScript =
+            {
+                Write-Verbose -Verbose "Getting DNS forwarding rule..."
+                $dnsFwdRule = Get-DnsServerForwarder -Verbose
+                if ($dnsFwdRule)
+                {
+                    Write-Verbose -Verbose "Removing DNS forwarding rule"
+                    Remove-DnsServerForwarder -IPAddress $dnsFwdRule.IPAddress -Force -Verbose
+                }
+                Write-Verbose -Verbose "End of UpdateDNSForwarder script..."
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = "[xADDomainController]BDC"
+        }
+#>
+        xPendingReboot RebootAfterPromotion {
+            Name = "RebootAfterDCPromotion"
+            DependsOn = "[xADDomainController]BDC"
+        }
+
     }
 }
